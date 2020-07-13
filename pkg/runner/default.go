982bc834660117a9dfc341877136546aed47dee5
Mzack9999 <mzack9999@protonmail.com>
2020-07-13 19:25:16 +0200

    removing fdmax as goreleaser doesn't support conditional compilation

--- pkg/runner/default.go
+++ pkg/runner/default.go
@@ -2,10 +2,10 @@ package runner
 
 const (
 	DefaultPortTimeoutSynScan     = 700
-	DefaultPortTimeoutConnectScan = 5000
+	DefaultPortTimeoutConnectScan = 10000
 
 	DefaultRateSynScan     = 1000
-	DefaultRateConnectScan = 15000
+	DefaultRateConnectScan = 1500
 
 	DefaultResolverThreads = 10
 
