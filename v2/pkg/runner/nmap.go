9f260e93c0fcb46175690ec9a2db26b3baa2f7b2
Hsmoy <1437095118@qq.com>
2022-04-11 22:26:47 +0800

    test

--- v2/pkg/runner/nmap.go
+++ v2/pkg/runner/nmap.go
@@ -17,38 +17,43 @@ func (r *Runner) handleNmap() error {
 	hasCLI := r.options.NmapCLI != ""
 	// If at least one is defined handle it
 	if command != "" {
-		args := strings.Split(command, " ")
-		var (
-			ips   []string
-			ports []string
-		)
-		allports := make(map[int]struct{})
+		
+	
 		for ip, p := range r.scanner.ScanResults.IPPorts {
-			ips = append(ips, ip)
+
+			args := strings.Split(command, " ")
+
+			allports := make(map[int]struct{})
+			var (
+				ports []string
+			)
+
+			//ips = append(ips, ip)
+
 			for pp := range p {
 				allports[pp] = struct{}{}
 			}
-		}
-		for p := range allports {
-			ports = append(ports, fmt.Sprintf("%d", p))
-		}
 
-		// if we have no open ports we avoid running nmap
-		if len(ports) == 0 {
-			errMsg := errors.New("Skipping nmap scan as no open ports were found")
-			gologger.Info().Msgf(errMsg.Error())
-			return errMsg
-		}
+			for p := range allports {
+				ports = append(ports, fmt.Sprintf("%d", p))
+			}
 
-		portsStr := strings.Join(ports, ",")
-		ipsStr := strings.Join(ips, ",")
+			// if we have no open ports we avoid running nmap
+			if len(ports) == 0 {
+				errMsg := errors.New("Skipping nmap scan as no open ports were found")
+				gologger.Info().Msgf(errMsg.Error())
+				return errMsg
+			}
 
-		args = append(args, "-p", portsStr)
-		args = append(args, ips...)
+			portsStr := strings.Join(ports, ",")
+			args = append(args, "-p", portsStr)
+			args = append(args, ip)	
 
-		// if requested via config file or via cli
+
+
+				// if requested via config file or via cli
 		if r.options.Nmap || hasCLI {
-			gologger.Info().Msgf("Running nmap command: %s -p %s %s", command, portsStr, ipsStr)
+			gologger.Info().Msgf("Running nmap command: %s -p %s %s", command, portsStr, ip)
 			cmd := exec.Command(args[0], args[1:]...)
 			cmd.Stdout = os.Stdout
 			err := cmd.Run()
@@ -58,9 +63,13 @@ func (r *Runner) handleNmap() error {
 				return errMsg
 			}
 		} else {
-			gologger.Info().Msgf("Suggested nmap command: %s -p %s %s", command, portsStr, ipsStr)
+			gologger.Info().Msgf("Suggested nmap command: %s -p %s %s", command, portsStr, ip)
 		}
-	}
 
+
+		}
+	
+	}
 	return nil
 }
+
