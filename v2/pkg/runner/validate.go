86dca82f28d3896d00dea1093750434332d02ff1
Mzack9999 <mzack9999@protonmail.com>
2022-06-25 15:58:12 +0200

    Adding host discovery (#346)
    * Adding host discovery
    
    - icmp ech
    - icmp timestamp
    
    * Adding ICMP Address Mask
    
    * Adding ARP scan
    
    * Adding tcp syn/ack hosts discovery
    
    * Add basic ndp discovery via ICMPv6
    
    * fixing merge errors
    
    * fixing lint issues
    
    * disabling wip probes + auto selection with -sn
    
    * adding missing function call
    
    * fixing deps chaos
    
    * implementing host discovery output
    
    * fixing tests
    
    * Add privileges check for host discovery

--- v2/pkg/runner/validate.go
+++ v2/pkg/runner/validate.go
@@ -111,6 +111,11 @@ func (options *Options) validateOptions() error {
 		options.SourcePort = port
 	}
 
+	// Host Discovery mode needs provileged access
+	if options.HostDiscovery && !privileges.IsPrivileged {
+		return errors.New("Host Discovery needs privileged access to manipulate raw packets")
+	}
+
 	return nil
 }
 
@@ -125,5 +130,24 @@ func (options *Options) configureOutput() {
 	}
 	if options.Silent {
 		gologger.DefaultLogger.SetMaxLevel(levels.LevelSilent)
+	}
+}
+
+// configureHostDiscovery enables default probes if none is specified
+// but host discovery option was requested
+func (options *Options) configureHostDiscovery() {
+	hasProbes := options.ArpPing || options.IPv6NeighborDiscoveryPing || options.IcmpAddressMaskRequestProbe ||
+		options.IcmpEchoRequestProbe || options.IcmpTimestampRequestProbe || len(options.TcpAckPingProbes) > 0 ||
+		len(options.TcpAckPingProbes) > 0
+	if options.HostDiscovery && !hasProbes {
+		// if no options were defined enable
+		// - ICMP Echo Request
+		// - ICMP timestamp
+		// - TCP SYN on port 80
+		// - TCP ACK on port 443
+		options.IcmpEchoRequestProbe = true
+		options.IcmpTimestampRequestProbe = true
+		options.TcpSynPingProbes = append(options.TcpSynPingProbes, "80")
+		options.TcpAckPingProbes = append(options.TcpAckPingProbes, "443")
 	}
 }
