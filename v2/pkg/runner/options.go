89ed0e3234aa22e47827b1d3ce2c78e2ba458fae
Mzack9999 <mzack9999@protonmail.com>
2022-07-12 20:36:19 +0200

    Updating goflags version and syntax (#387)
    * Updating goflags version and syntax
    
    * removes mod init
    
    * fixing gh syntax

--- v2/pkg/runner/options.go
+++ v2/pkg/runner/options.go
@@ -27,32 +27,32 @@ type Options struct {
 	Nmap           bool // Invoke nmap detailed scan on results
 	InterfacesList bool // InterfacesList show interfaces list
 
-	Retries           int                           // Retries is the number of retries for the port
-	Rate              int                           // Rate is the rate of port scan requests
-	Timeout           int                           // Timeout is the seconds to wait for ports to respond
-	WarmUpTime        int                           // WarmUpTime between scan phases
-	Host              goflags.NormalizedStringSlice // Host is the single host or comma-separated list of hosts to find ports for
-	HostsFile         string                        // HostsFile is the file containing list of hosts to find port for
-	Output            string                        // Output is the file to write found ports to.
-	Ports             string                        // Ports is the ports to use for enumeration
-	PortsFile         string                        // PortsFile is the file containing ports to use for enumeration
-	ExcludePorts      string                        // ExcludePorts is the list of ports to exclude from enumeration
-	ExcludeIps        string                        // Ips or cidr to be excluded from the scan
-	ExcludeIpsFile    string                        // File containing Ips or cidr to exclude from the scan
-	TopPorts          string                        // Tops ports to scan
-	SourceIP          string                        // SourceIP to use in TCP packets
-	SourcePort        string                        // Source Port to use in packets
-	Interface         string                        // Interface to use for TCP packets
-	ConfigFile        string                        // Config file contains a scan configuration
-	NmapCLI           string                        // Nmap command (has priority over config file)
-	Threads           int                           // Internal worker threads
-	EnableProgressBar bool                          // Enable progress bar
-	ScanAllIPS        bool                          // Scan all the ips
-	IPVersion         goflags.NormalizedStringSlice // IP Version to use while resolving hostnames
-	ScanType          string                        // Scan Type
-	Proxy             string                        // Socks5 proxy
-	ProxyAuth         string                        // Socks5 proxy authentication (username:password)
-	Resolvers         string                        // Resolvers (comma separated or file)
+	Retries           int                 // Retries is the number of retries for the port
+	Rate              int                 // Rate is the rate of port scan requests
+	Timeout           int                 // Timeout is the seconds to wait for ports to respond
+	WarmUpTime        int                 // WarmUpTime between scan phases
+	Host              goflags.StringSlice // Host is the single host or comma-separated list of hosts to find ports for
+	HostsFile         string              // HostsFile is the file containing list of hosts to find port for
+	Output            string              // Output is the file to write found ports to.
+	Ports             string              // Ports is the ports to use for enumeration
+	PortsFile         string              // PortsFile is the file containing ports to use for enumeration
+	ExcludePorts      string              // ExcludePorts is the list of ports to exclude from enumeration
+	ExcludeIps        string              // Ips or cidr to be excluded from the scan
+	ExcludeIpsFile    string              // File containing Ips or cidr to exclude from the scan
+	TopPorts          string              // Tops ports to scan
+	SourceIP          string              // SourceIP to use in TCP packets
+	SourcePort        string              // Source Port to use in packets
+	Interface         string              // Interface to use for TCP packets
+	ConfigFile        string              // Config file contains a scan configuration
+	NmapCLI           string              // Nmap command (has priority over config file)
+	Threads           int                 // Internal worker threads
+	EnableProgressBar bool                // Enable progress bar
+	ScanAllIPS        bool                // Scan all the ips
+	IPVersion         goflags.StringSlice // IP Version to use while resolving hostnames
+	ScanType          string              // Scan Type
+	Proxy             string              // Socks5 proxy
+	ProxyAuth         string              // Socks5 proxy authentication (username:password)
+	Resolvers         string              // Resolvers (comma separated or file)
 	baseResolvers     []string
 	OnResult          OnResultCallback // OnResult callback
 	CSV               bool
@@ -90,7 +90,7 @@ func ParseOptions() *Options {
 	flagSet.SetDescription(`Naabu is a port scanning tool written in Go that allows you to enumerate open ports for hosts in a fast and reliable manner.`)
 
 	flagSet.CreateGroup("input", "Input",
-		flagSet.NormalizedStringSliceVarP(&options.Host, "host", "", []string{}, "hosts to scan ports for (comma-separated)"),
+		flagSet.StringSliceVarP(&options.Host, "host", "", nil, "hosts to scan ports for (comma-separated)", goflags.NormalizedStringSliceOptions),
 		flagSet.StringVarP(&options.HostsFile, "l", "list", "", "list of hosts to scan ports (file)"),
 		flagSet.StringVarP(&options.ExcludeIps, "eh", "exclude-hosts", "", "hosts to exclude from the scan (comma-separated)"),
 		flagSet.StringVarP(&options.ExcludeIpsFile, "ef", "exclude-file", "", "list of hosts to exclude from scan (file)"),
@@ -118,7 +118,7 @@ func ParseOptions() *Options {
 
 	flagSet.CreateGroup("config", "Configuration",
 		flagSet.BoolVarP(&options.ScanAllIPS, "sa", "scan-all-ips", false, "scan all the IP's associated with DNS record"),
-		flagSet.NormalizedStringSliceVarP(&options.IPVersion, "iv", "ip-version", []string{}, "ip version to scan of hostname (4,6) - (default 4)"),
+		flagSet.StringSliceVarP(&options.IPVersion, "iv", "ip-version", nil, "ip version to scan of hostname (4,6) - (default 4)", goflags.NormalizedStringSliceOptions),
 		flagSet.StringVarP(&options.ScanType, "s", "scan-type", SynScan, "type of port scan (SYN/CONNECT)"),
 		flagSet.StringVar(&options.SourceIP, "source-ip", "", "source ip and port (x.x.x.x:yyy)"),
 		flagSet.BoolVarP(&options.InterfacesList, "il", "interface-list", false, "list available interfaces and public ip"),
@@ -137,17 +137,18 @@ func ParseOptions() *Options {
 
 	flagSet.CreateGroup("host-discovery", "Host-Discovery",
 		flagSet.BoolVarP(&options.HostDiscovery, "host-discvoery", "sn", false, "Run Host Discovery scan"),
-		flagSet.StringSliceVarP(&options.TcpSynPingProbes, "probe-tcp-syn", "ps", nil, "TCP SYN Ping"),
-		flagSet.StringSliceVarP(&options.TcpAckPingProbes, "probe-tcp-ack", "pa", nil, "TCP ACK Ping"),
+		flagSet.StringSliceVarP(&options.TcpSynPingProbes, "probe-tcp-syn", "ps", nil, "TCP SYN Ping", goflags.StringSliceOptions),
+		flagSet.StringSliceVarP(&options.TcpAckPingProbes, "probe-tcp-ack", "pa", nil, "TCP ACK Ping", goflags.StringSliceOptions),
 		flagSet.BoolVarP(&options.IcmpEchoRequestProbe, "probe-icmp-echo", "pe", false, "ICMP echo request Ping"),
 		flagSet.BoolVarP(&options.IcmpTimestampRequestProbe, "probe-icmp-timestamp", "pp", false, "ICMP timestamp request Ping"),
 		flagSet.BoolVarP(&options.IcmpAddressMaskRequestProbe, "probe-icmp-address-mask", "pm", false, "ICMP address mask request Ping"),
 		flagSet.BoolVarP(&options.ArpPing, "arp-ping", "arp", false, "ARP ping"),
 		flagSet.BoolVarP(&options.IPv6NeighborDiscoveryPing, "nd-ping", "nd", false, "IPv6 Neighbor Discovery"),
-	// flagSet.StringSliceVarP(&options.IpProtocolPingProbes, "probe-ip-protocol", "po", []string{}, "IP Protocol Ping"),
-	// flagSet.StringSliceVarP(&options.UdpPingProbes, "probe-udp", "pu", []string{}, "UDP Ping"),
-	// flagSet.StringSliceVarP(&options.STcpInitPingProbes, "probe-stcp-init", "py", []string{}, "SCTP INIT Ping"),
-	// flagSet.BoolVarP(&options.HostDiscoveryIgnoreRST, "discovery-ignore-rst", "irst", false, "Ignore RST packets during host discovery"),
+		// The following flags are left as placeholder
+		// flagSet.StringSliceVarP(&options.IpProtocolPingProbes, "probe-ip-protocol", "po", []string{}, "IP Protocol Ping"),
+		// flagSet.StringSliceVarP(&options.UdpPingProbes, "probe-udp", "pu", []string{}, "UDP Ping"),
+		// flagSet.StringSliceVarP(&options.STcpInitPingProbes, "probe-stcp-init", "py", []string{}, "SCTP INIT Ping"),
+		// flagSet.BoolVarP(&options.HostDiscoveryIgnoreRST, "discovery-ignore-rst", "irst", false, "Ignore RST packets during host discovery"),
 	)
 
 	flagSet.CreateGroup("optimization", "Optimization",
