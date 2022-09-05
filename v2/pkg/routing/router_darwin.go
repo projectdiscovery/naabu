14d489ee853f549faba33037eaaa84b34854c3b1
mzack <marco.rivoli.nvh@gmail.com>
2022-09-05 20:59:01 +0200

    attempting to infer route type

--- v2/pkg/routing/router_darwin.go
+++ v2/pkg/routing/router_darwin.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
+	"github.com/projectdiscovery/gologger"
 	"github.com/projectdiscovery/sliceutil"
 	"github.com/projectdiscovery/stringsutil"
 	"go.uber.org/multierr"
@@ -64,6 +65,8 @@ func New() (Router, error) {
 		return nil, multierr.Combine(err, errOutboundIps)
 	}
 
+	var lastType RouteType
+
 	scanner := bufio.NewScanner(bytes.NewReader(netstatOutput))
 	for scanner.Scan() {
 		outputLine := strings.TrimSpace(scanner.Text())
@@ -98,8 +101,16 @@ func New() (Router, error) {
 			case hasSemicolon:
 				route.Type = IPv6
 			default:
-				return nil, fmt.Errorf("unknown route type: '%s'", outputLine)
+				// use last route type and print a warning
+				if lastType != "" {
+					gologger.Debug().Msgf("using '%s' for unknown route type: '%s'\n", lastType, outputLine)
+					route.Type = lastType
+				} else {
+					// we can't determine the route type
+					return nil, fmt.Errorf("could not determine route type for: '%s'", outputLine)
+				}
 			}
+			lastType = route.Type
 			routes = append(routes, route)
 		}
 	}
