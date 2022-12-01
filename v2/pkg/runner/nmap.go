477d9bd4c55612656b46a5cf2bb20f41baa87e63
xm1k3 <xm1k3@protonmail.com>
2022-12-01 18:01:25 +0100

    Issue with nmap flag input (#487)
    * managed with nmap binary
    
    * better management for nmap + check on win
    
    * testcases
    
    * fix on log

--- v2/pkg/runner/nmap.go
+++ v2/pkg/runner/nmap.go
@@ -86,7 +86,22 @@ func (r *Runner) handleNmap() error {
 			// if requested via config file or via cli
 			if (r.options.Nmap || hasCLI) && commandCanBeExecuted {
 				gologger.Info().Msgf("Running nmap command: %s -p %s %s", command, portsStr, ipsStr)
-				cmd := exec.Command(args[0], args[1:]...)
+				// check when user type '-nmap-cli "nmap -sV"'
+				// automatically remove nmap
+				posArgs := 0
+				// nmapCommand helps to check if user is on a Windows machine
+				nmapCommand := "nmap"
+				if args[0] == "nmap" || args[0] == "nmap.exe" {
+					posArgs = 1
+				}
+
+				// if it's windows search for the executable
+				if isWindows() {
+					nmapCommand = "nmap.exe"
+				}
+
+				cmd := exec.Command(nmapCommand, args[posArgs:]...)
+
 				cmd.Stdout = os.Stdout
 				err := cmd.Run()
 				if err != nil {
