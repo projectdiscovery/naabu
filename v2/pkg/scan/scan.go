1131dac75f3387b878c2dc12553398d69f4d9eab
jpts <james@cleverley-prance.uk>
2023-03-15 17:44:07 +0000

    feat: Add timeout to scan through SOCKS proxy

--- v2/pkg/scan/scan.go
+++ v2/pkg/scan/scan.go
@@ -1,6 +1,7 @@
 package scan
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -558,7 +559,9 @@ func (s *Scanner) ConnectPort(host string, p *port.Port, timeout time.Duration) 
 		conn net.Conn
 	)
 	if s.proxyDialer != nil {
-		conn, err = s.proxyDialer.Dial(p.Protocol.String(), hostport)
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		conn, err = s.proxyDialer.(proxy.ContextDialer).DialContext(ctx, p.Protocol.String(), hostport)
 		if err != nil {
 			return false, err
 		}
