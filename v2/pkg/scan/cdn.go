3a451e88b8f9cbd1dcbf4241250f419a1f855763
Joshua Martinelle <contact@jomar.fr>
2023-08-02 12:05:25 +0200

    Check if IP is WAF

--- v2/pkg/scan/cdn.go
+++ v2/pkg/scan/cdn.go
@@ -15,5 +15,15 @@ func (s *Scanner) CdnCheck(ip string) (bool, string, error) {
 	if !iputil.IsIP(ip) {
 		return false, "", errors.Errorf("%s is not a valid ip", ip)
 	}
+
+	matched, val, err := s.cdn.CheckWAF(net.ParseIP((ip)))
+	if err != nil {
+		return false, "", err
+	}
+
+	if matched {
+		return matched, val, err
+	}
+
 	return s.cdn.CheckCDN(net.ParseIP((ip)))
 }
