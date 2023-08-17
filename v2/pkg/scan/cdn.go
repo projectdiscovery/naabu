ebfecbae40dc4c3f0226a9a526b9cf7e0f0682b2
JoMar <contact@jomar.fr>
2023-08-17 18:28:05 +0200

    Exclude waf (#749)
    * Check if IP is WAF
    
    * Update doc
    
    * minor change use .Check() method
    
    ---------
    
    Co-authored-by: Sandeep Singh <sandeep@projectdiscovery.io>
    Co-authored-by: Tarun Koyalwar <tarun@projectdiscovery.io>

--- v2/pkg/scan/cdn.go
+++ v2/pkg/scan/cdn.go
@@ -7,7 +7,7 @@ import (
 	iputil "github.com/projectdiscovery/utils/ip"
 )
 
-// CdnCheck verifies if the given ip is part of Cdn ranges
+// CdnCheck verifies if the given ip is part of Cdn/WAF ranges
 func (s *Scanner) CdnCheck(ip string) (bool, string, error) {
 	if s.cdn == nil {
 		return false, "", errors.New("cdn client not initialized")
@@ -15,5 +15,12 @@ func (s *Scanner) CdnCheck(ip string) (bool, string, error) {
 	if !iputil.IsIP(ip) {
 		return false, "", errors.Errorf("%s is not a valid ip", ip)
 	}
-	return s.cdn.CheckCDN(net.ParseIP((ip)))
+
+	// the goal is to check if ip is part of cdn/waf to decide if target should be scanned or not
+	// since 'cloud' itemtype does not fit logic here , we consider target is not part of cdn/waf
+	matched, value, itemType, err := s.cdn.Check(net.ParseIP((ip)))
+	if itemType == "cloud" {
+		return false, "", err
+	}
+	return matched, value, err
 }
