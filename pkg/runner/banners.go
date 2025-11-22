813ffa59f98b70911cdc93573e22e08ac7a10a16
PDTeamX <8293321+ehsandeep@users.noreply.github.com>
2025-11-23 05:28:36 +0530

    version update

--- pkg/runner/banners.go
+++ pkg/runner/banners.go
@@ -20,7 +20,7 @@ const banner = `
 `
 
 // Version is the current Version of naabu
-const Version = `2.3.6`
+const Version = `2.3.7`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
