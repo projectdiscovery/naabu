fc631626b5e0cef02ebfc9d520378c7d4fa1064f
nu11z <55335158+nu11zy@users.noreply.github.com>
2026-03-26 16:03:44 +0300

    Parse Darwin routing table with golang.org/x/net/route (#1643)
    * parse Darwin routing table with golang.org/x/net/route
    
    * add subnet mask to destination
    
    * add LinkAddr support
    
    * consolidate darwin and freebsd routing logic into one file
    
    * use windows api to get routing table instead of os.exec
    
    * fix potential nil pointer dereference
    
    * fixing minor issues
    
    ---------
    
    Co-authored-by: Mzack9999 <mzack9999@protonmail.com>

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/projectdiscovery/mapcidr v1.1.97
 	github.com/projectdiscovery/networkpolicy v0.1.35
 	github.com/projectdiscovery/ratelimit v0.0.84
+	github.com/projectdiscovery/retryabledns v1.0.114
 	github.com/projectdiscovery/retryablehttp-go v1.3.6
 	github.com/projectdiscovery/uncover v1.2.0
 	github.com/projectdiscovery/utils v0.10.0
@@ -96,7 +97,6 @@ require (
 	github.com/projectdiscovery/fastdialer v0.5.5 // indirect
 	github.com/projectdiscovery/hmap v0.0.100 // indirect
 	github.com/projectdiscovery/machineid v0.0.0-20250715113114-c77eb3567582 // indirect
-	github.com/projectdiscovery/retryabledns v1.0.114 // indirect
 	github.com/refraction-networking/utls v1.8.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
--- pkg/runner/healthcheck.go
+++ pkg/runner/healthcheck.go
@@ -15,11 +15,11 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	// RW permissions on config file
 	cfgFilePath, _ := flagSet.GetConfigFilePath()
 	var test strings.Builder
-	test.WriteString(fmt.Sprintf("Version: %s\n", Version))
-	test.WriteString(fmt.Sprintf("Operative System: %s\n", runtime.GOOS))
-	test.WriteString(fmt.Sprintf("Architecture: %s\n", runtime.GOARCH))
-	test.WriteString(fmt.Sprintf("Go Version: %s\n", runtime.Version()))
-	test.WriteString(fmt.Sprintf("Compiler: %s\n", runtime.Compiler))
+	_, _ = fmt.Fprintf(&test, "Version: %s\n", Version)
+	_, _ = fmt.Fprintf(&test, "Operative System: %s\n", runtime.GOOS)
+	_, _ = fmt.Fprintf(&test, "Architecture: %s\n", runtime.GOARCH)
+	_, _ = fmt.Fprintf(&test, "Go Version: %s\n", runtime.Version())
+	_, _ = fmt.Fprintf(&test, "Compiler: %s\n", runtime.Compiler)
 
 	var testResult string
 	if privileges.IsPrivileged {
@@ -27,7 +27,7 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	} else {
 		testResult = "Ko"
 	}
-	test.WriteString(fmt.Sprintf("Privileged/NET_RAW: %s\n", testResult))
+	_, _ = fmt.Fprintf(&test, "Privileged/NET_RAW: %s\n", testResult)
 
 	ok, err := fileutil.IsReadable(cfgFilePath)
 	if ok {
@@ -38,7 +38,7 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	if err != nil {
 		testResult += fmt.Sprintf(" (%s)", err)
 	}
-	test.WriteString(fmt.Sprintf("Config file \"%s\" Read => %s\n", cfgFilePath, testResult))
+	_, _ = fmt.Fprintf(&test, "Config file \"%s\" Read => %s\n", cfgFilePath, testResult)
 	ok, err = fileutil.IsWriteable(cfgFilePath)
 	if ok {
 		testResult = "Ok"
@@ -48,7 +48,7 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	if err != nil {
 		testResult += fmt.Sprintf(" (%s)", err)
 	}
-	test.WriteString(fmt.Sprintf("Config file \"%s\" Write => %s\n", cfgFilePath, testResult))
+	_, _ = fmt.Fprintf(&test, "Config file \"%s\" Write => %s\n", cfgFilePath, testResult)
 	c4, err := net.Dial("tcp4", "scanme.sh:80")
 	if err == nil && c4 != nil {
 		_ = c4.Close()
@@ -57,7 +57,7 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	if err != nil {
 		testResult = fmt.Sprintf("Ko (%s)", err)
 	}
-	test.WriteString(fmt.Sprintf("TCP IPv4 connectivity to scanme.sh:80 => %s\n", testResult))
+	_, _ = fmt.Fprintf(&test, "TCP IPv4 connectivity to scanme.sh:80 => %s\n", testResult)
 	c6, err := net.Dial("tcp6", "scanme.sh:80")
 	if err == nil && c6 != nil {
 		_ = c6.Close()
@@ -66,7 +66,7 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	if err != nil {
 		testResult = fmt.Sprintf("Ko (%s)", err)
 	}
-	test.WriteString(fmt.Sprintf("TCP IPv6 connectivity to scanme.sh:80 => %s\n", testResult))
+	_, _ = fmt.Fprintf(&test, "TCP IPv6 connectivity to scanme.sh:80 => %s\n", testResult)
 	u4, err := net.Dial("udp4", "scanme.sh:53")
 	if err == nil && u4 != nil {
 		_ = u4.Close()
@@ -75,7 +75,7 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	if err != nil {
 		testResult = fmt.Sprintf("Ko (%s)", err)
 	}
-	test.WriteString(fmt.Sprintf("UDP IPv4 connectivity to scanme.sh:80 => %s\n", testResult))
+	_, _ = fmt.Fprintf(&test, "UDP IPv4 connectivity to scanme.sh:80 => %s\n", testResult)
 	u6, err := net.Dial("udp6", "scanme.sh:80")
 	if err == nil && u6 != nil {
 		_ = u6.Close()
@@ -84,7 +84,7 @@ func DoHealthCheck(options *Options, flagSet *goflags.FlagSet) string {
 	if err != nil {
 		testResult = fmt.Sprintf("Ko (%s)", err)
 	}
-	test.WriteString(fmt.Sprintf("UDP IPv6 connectivity to scanme.sh:80 => %s\n", testResult))
+	_, _ = fmt.Fprintf(&test, "UDP IPv6 connectivity to scanme.sh:80 => %s\n", testResult)
 
 	return test.String()
 }
