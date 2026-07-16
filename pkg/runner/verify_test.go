42a14c0a87c5679f6f08bdfa59b6f832f173c8d6
Mzack9999 <mzack9999@protonmail.com>
2026-07-16 16:54:41 +0400

    fix lint

--- pkg/runner/verify_test.go
+++ pkg/runner/verify_test.go
@@ -17,7 +17,7 @@ import (
 func TestConnectVerificationBoundedAndVerifies(t *testing.T) {
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
-	defer ln.Close()
+	defer func() { _ = ln.Close() }()
 	go func() {
 		for {
 			conn, err := ln.Accept()
